16ad850f0ebe3257954fcafe9559bd7631be55a2
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2022-03-17 11:52:44 -0500

    automated whole process

--- automate_test.py
+++ automate_test.py
@@ -1,13 +1,35 @@
 import os
 import generate_feature_vector as gf
+import docxTOtxt as dt
+import readFromTranscription as rt
 
-def main():
-    folder = input("Enter folder path: ")
-    files_in_folder = os.listdir(folder)
-    for file in files_in_folder:
-        temp = folder + '/' + file
-        gf.run_remote(str(temp))
+path = '/Users/ankit/Documents/SSPA Project/CSV Files/Bipolar Disorder_scene2/'
+
+files = os.listdir(path)
 
 
-if __name__ == '__main__':
-    main()
+def text_maker(filepath):
+    files = os.listdir(path)
+    for f in files:
+        temp = path + f
+        dt.process(temp)
+
+
+def csv_maker(filepath):
+    files = os.listdir(filepath)
+    for f in files:
+        temp = path + f
+        rt.remote(temp)
+
+
+def json_maker(filepath, label):
+
+    files = os.listdir(filepath)
+    for f in files:
+        temp = path + f
+        try:
+            gf.makeJSON(temp, label)
+        except ZeroDivisionError:
+            print(f)
+    
+

7972c8b983041dc7101ab63fdf81504d04f91c14
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2022-03-17 11:54:48 -0500

    Updated Comments

--- automate_test.py
+++ automate_test.py
@@ -30,6 +30,9 @@ def json_maker(filepath, label):
         try:
             gf.makeJSON(temp, label)
         except ZeroDivisionError:
+            #Because if there are inconsistencies in the doc file - that will flow down to the final csv file being blank - because regex won't work
+            #This means JSON will incur a zero division error trying to process logarithmic and division formulae using a blank string (NULL VALUE)
+            #At that point we return the value of the iterator - which has the file name in it - and helps us understand which file has inonsistencies
             print(f)
-    
+
 

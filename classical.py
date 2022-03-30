4bdf54655c15d64f0c03c3fbc3aea1c589376e33
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2022-03-30 11:51:01 -0500

    Updated iterations - converges at 90% accuracy

--- classical.py
+++ classical.py
@@ -9,57 +9,78 @@ import pandas as pd
 import numpy as np
 
 
-X_train = X_train.astype(np.float)
-Y_train = Y_train.astype(np.float)
-
-
-rf = RandomForestClassifier()
-knn = KNeighborsClassifier()
-dtc = DecisionTreeClassifier()
-lr = LogisticRegression()
-rc = RidgeClassifier()
-svm = LinearSVC(multi_class='crammer_singer')
-
-test_size = [0.05, 0.1, 0.15, 0.20, 0.25, 0.5]
-
-
-randomForest = []
-KNN = []
-decisionTree = []
-logisticRegression = []
-ridgeClassifer = []
-supportVectorMachine = []
-
-#if you wish to split test sizes then use a loop at this point and typesongs
-#for i in test_size
-#and split your data accordingly between train and test
-#this is not required for dreaddit since we already have it split
-#this is just FYI
-
-yPredR = rf.fit(X_train,Y_train).predict(X_test)
-yPredK = knn.fit(X_train, Y_train).predict(X_test)
-yPredDT = dtc.fit(X_train, Y_train).predict(X_test)
-yPredLR = lr.fit(X_train, Y_train).predict(X_test)
-yPredRC = rc.fit(X_train, Y_train).predict(X_test)
-yPredSVM = svm.fit(X_train, Y_train).predict(X_test)
+data = pd.read_csv('/Users/ankit/Documents/SSPA Project/Data_Building_Codes/bpd_schizo.csv')
+X_train = data.iloc[:, 0:26]
+Y_train = data['label']
 
 
 
-randomForest.append(1-((yPredR!=Y_test).sum())/np.shape(Y_test)[0])
-KNN.append(1-(yPredK!=Y_test).sum()/np.shape(Y_test)[0])
-decisionTree.append(1-(yPredDT!=Y_test).sum()/np.shape(Y_test)[0])
-logisticRegression.append(1-(yPredLR!=Y_test).sum()/np.shape(Y_test)[0])
-ridgeClassifer.append(1-(yPredRC!=Y_test).sum()/np.shape(Y_test)[0])
-supportVectorMachine.append(1-(yPredSVM!=Y_test).sum()/np.shape(Y_test)[0])
+def classify(X_train, Y_train):
+
+
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+
+    scale = StandardScaler()
+    scale.fit(X_train)
+    X_train = scale.transform(X_train)
+
+
+    #X_train = X_train.astype(np.float)
+    #Y_train = Y_train.astype(np.float)
+
+
+    rf = RandomForestClassifier()
+    knn = KNeighborsClassifier()
+    dtc = DecisionTreeClassifier()
+    lr = LogisticRegression()
+    rc = RidgeClassifier()
+    svm = LinearSVC()
+
+    test_size = [0.05, 0.1, 0.15, 0.20, 0.25, 0.5]
+
+
+    randomForest = []
+    KNN = []
+    decisionTree = []
+    logisticRegression = []
+    ridgeClassifer = []
+    supportVectorMachine = []
+
+    #if you wish to split test sizes then use a loop at this point and typesongs
+    #for i in test_size
+    #and split your data accordingly between train and test
+    #this is just FYI
+
+    for i in test_size:
+
+        X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=i)
+
+        yPredR = rf.fit(X_train,Y_train).predict(X_test)
+        yPredK = knn.fit(X_train, Y_train).predict(X_test)
+        yPredDT = dtc.fit(X_train, Y_train).predict(X_test)
+        yPredLR = lr.fit(X_train, Y_train).predict(X_test)
+        yPredRC = rc.fit(X_train, Y_train).predict(X_test)
+        yPredSVM = svm.fit(X_train, Y_train).predict(X_test)
+
+
+
+        randomForest.append(1-((yPredR!=Y_test).sum())/np.shape(Y_test)[0])
+        KNN.append(1-(yPredK!=Y_test).sum()/np.shape(Y_test)[0])
+        decisionTree.append(1-(yPredDT!=Y_test).sum()/np.shape(Y_test)[0])
+        logisticRegression.append(1-(yPredLR!=Y_test).sum()/np.shape(Y_test)[0])
+        ridgeClassifer.append(1-(yPredRC!=Y_test).sum()/np.shape(Y_test)[0])
+        supportVectorMachine.append(1-(yPredSVM!=Y_test).sum()/np.shape(Y_test)[0])
 
 
 
 
 
 
-print('Random Forest', randomForest)
-print('KNN', KNN)
-print('Decistion Tree', decisionTree)
-print('Logistic Regression', logisticRegression)
-print('Ridge Classifier', ridgeClassifer)
-print('Support Vector Machine', supportVectorMachine)
+    print('Random Forest', randomForest)
+    print('KNN', KNN)
+    print('Decistion Tree', decisionTree)
+    print('Logistic Regression', logisticRegression)
+    print('Ridge Classifier', ridgeClassifer)
+    print('Support Vector Machine', supportVectorMachine)
+

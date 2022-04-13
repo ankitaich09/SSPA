014fbe5d0ac32c1f197cf7872e661f381bc01653
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2022-04-13 13:41:54 -0500

    Add files via upload

--- liwc_string_level.py
+++ liwc_string_level.py
@@ -1,4 +1,6 @@
 import subprocess
+import pandas as pd
+import time
 
 input_string = "Hi, I'm your new neighbor, my name is Baal Bichi, please help me with the clinical task"
 
@@ -11,3 +13,11 @@ cmd_to_execute = ["LIWC-22-cli",
                   "--output", output_loc]
 
 subprocess.call(cmd_to_execute)
+
+
+time.sleep(2)
+
+#adding a 2 second wait - to let each process finish
+
+temp_df = pd.read_json(output_loc, lines=True)
+#this contains the LIWC output of the string that was just processed. 

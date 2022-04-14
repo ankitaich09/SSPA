3c127507b273b9a0657c9d05f50bffa7efcb67bd
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2022-04-14 16:10:47 -0500

    Update liwc_string_level.py

--- liwc_string_level.py
+++ liwc_string_level.py
@@ -2,23 +2,3 @@ import subprocess
 import pandas as pd
 import time
 
-input_string = "Hi, I'm your new neighbor, my name is Baal Bichi, please help me with the clinical task"
-
-output_loc = '/Users/ankit/Documents/SSPA Project/Analysis_Codes/sample_result.ndjson'
-
-cmd_to_execute = ["LIWC-22-cli",
-                  "--mode", "wc",
-                  "--input", "console",
-                  "--console-text", input_string,
-                  "--output", output_loc]
-
-subprocess.call(cmd_to_execute)
-
-
-time.sleep(2)
-
-#adding a 2 second wait - to let each process finish
-
-temp_df = pd.read_json(output_loc, lines=True)
-#this contains the LIWC output of the string that was just processed.
-#this is as a dataframe

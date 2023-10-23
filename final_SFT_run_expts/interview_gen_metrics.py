4b4ffaf3a741787f234e6e1049ad4aeda55c44e6
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2023-10-23 11:39:45 -0500

    Add files via upload

--- final_SFT_run_expts/interview_gen_metrics.py
+++ final_SFT_run_expts/interview_gen_metrics.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from rouge_score import rouge_scorer
+import os
 from evaluate import load
 from sentence_transformers import SentenceTransformer, util
 
@@ -21,14 +22,32 @@ def cosine_sim(str_1, str_2):
 def rouge(str_1, str_2):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
     scores = scorer.score(str_1, str_2)
-
+    #access scores as
+    # scores['rouge1'].precision or recall or fmeasure
     return scores
 
 def bert(list_1, list_2):
 
+    #pass two strings as list objects
+    #or pass two lists of strings - if using this - use a mean function somewhere
     bertscore = load("bertscore")
     predictions = list_1
     references =list_2
     results = bertscore.compute(predictions=predictions, references=references, lang="en", model_type='microsoft/deberta-large-mnli')
-    return results
+    return results['precision'][0], results['recall'][0], results['f1'][0]
 
+def process(filepath):
+    i=0
+    scores = []
+    df = pd.read_csv(filepath)
+    actual = list(df['GOLD'])
+    predicted = list(df['PREDICTED'])
+    for k in range(len(actual)):
+        try:
+            scores.append(cosine_sim(actual[k], predicted[k]))
+            print('Line Number', i)
+        except:
+            pass
+        i = i+ 1
+
+    return scores
